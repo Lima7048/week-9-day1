21f9e7f6f8e43839b3b751d5ac6d67b968fdad9d
salima <brycesalima@gmail.com>
2024-03-04 16:21:16 +0000

    update deploy

--- src/app/component/Popover.js
+++ src/app/component/Popover.js
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import * as Popover from "@radix-ui/react-popover";
 // import "popover.css"
@@ -7,7 +8,7 @@ const PopoverDemo = () => (
         <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
         <Popover.Portal>
             <Popover.Content className="PopoverContent" sideOffset={5}>
-                Some more info… You can find the full website @
+                Some more info… You can find the full website @ https://week-9-day1.vercel.app/
                 <Popover.Arrow className="PopoverArrow" />
             </Popover.Content>
         </Popover.Portal>
